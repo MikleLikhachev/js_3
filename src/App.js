cc6a94aa84076c92b0112e1b5a8f0a97f24b838c
MikleLikhachev <miklelikhachev@gmail.com>
2023-05-09 10:13:38 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Checkbox,
+} from '@material-ui/core';
+import { Delete as DeleteIcon } from '@material-ui/icons';
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: 600,
+    margin: 'auto',
+    textAlign: 'center',
+  },
+  heading: {
+    margin: theme.spacing(2),
+  },
+  list: {
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function CardList() {
+  const classes = useStyles();
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+        .then(response => setCards(response.data))
+        .catch(error => console.log(error));
+  }, []);
+
+  const handleToggle = (id) => {
+    const updatedCards = cards.map(card => {
+      if (card.id === id) {
+        card.completed = !card.completed;
+      }
+      return card;
+    });
+    setCards(updatedCards);
+  };
+
+  const handleDelete = (id) => {
+    setCards(cards.filter(card => card.id !== id))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={classes.root}>
+        <h1 className={classes.heading}>TODOшки</h1>
+        <List className={classes.list}>
+          {cards.map(card => (
+              <ListItem key={card.id} dense button onClick={() => handleToggle(card.id)}>
+                <Checkbox checked={card.completed} disableRipple />
+                <ListItemText primary={card.title} />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(card.id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+          ))}
+        </List>
+      </div>
   );
 }
 
-export default App;
+export default CardList;
